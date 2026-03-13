90fe9142b343e0130c6345df643857105e5e2fb9
tushar goswami <tussygoswami01@gmail.com>
2026-03-13 11:39:22 +0530

    patterns problems

---
+++ Day_2/Patterns_questions/patterns_part_3.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+int n=1;
+
+for(int row=6;row>0;row--){
+
+
+for(int s=0;s<row-1;s++){
+
+
+    cout<<"  ";
+}
+
+for(int j=0;j<n;j++){
+    cout<<"* ";
+    cout<<" ";
+    
+}
+
+
+cout<<endl;
+n=n+1;
+
+}
+
+
+
+
+
+}
