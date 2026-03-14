a44e1db12a0fd78e0ed07888d250a5b97b423b2f
tushar goswami <tussygoswami01@gmail.com>
2026-03-14 23:31:55 +0530

    diamond pattern

---
+++ Day_3/diamond_pattern.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n=4;
+int p=0;
+int f=0;
+int o=9;
+for(int i=0;i<5;i++){
+
+
+for(int j =0;j<n;j++){
+
+cout<<"  ";
+
+}
+
+
+for(int k=0;k<=p;k++){
+
+    cout<<"* ";
+}
+
+
+
+
+
+
+
+cout<<endl;
+n=n-1;
+p=p+2;
+
+
+}
+
+
+for(int row=0;row<5;row++){
+
+
+
+for(int b=0;b<row;b++){
+
+    cout<<"  ";
+}
+
+
+for(int z=0;z<o;z++){
+
+    cout<<"* ";
+}
+
+
+o=o-2;
+cout<<endl;
+
+}
+
+
+
+
+
+
+}
